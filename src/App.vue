d71f2a67fef011dcfe508bdffb4fd5ab7a6dc24c
Sado-Shi <derasado04@gmail.com>
2022-01-01 23:28:09 +0900

    コメントアウト

--- src/App.vue
+++ src/App.vue
@@ -20,32 +20,32 @@ const array = [1, 2, 3];
 // const newArray = array.map(el => {
 //   return el * 2
 // })
-const newArray = array.map((el) => ({ value: el * 2 }));
+const newArray = array.map(el => el * 2);
 console.log(newArray);
 // array.push(6, 7);
 // console.log(array);
-// array.length = [0];
+// array.length = [0];vue
 // console.log(array);
-const insert = "insert";
-const text = "string" + insert + 1;
-console.log(text);
+// const insert = "insert";
+// const text = "string" + insert + 1;
+// console.log(text);
 
-const ata = "省略";
-const bta = "記法";
-const newObject = { ata, bta };
-console.log(newObject);
+// const ata = "省略";
+// const bta = "記法";
+// const newObject = { ata, bta };
+// console.log(newObject);
 
-const [test, dayo] = [4, 6];
-console.log(test, dayo);
+// const [test, dayo] = [4, 6];
+// console.log(test, dayo);
 
-const { id } = { id: 1, name: "りんご" };
-console.log(id);
+// const { id } = { id: 1, name: "りんご" };
+// console.log(id);
 
-function testFunc({ id, name }) {
-  console.log(id);
-  console.log(name);
-}
-testFunc({ id: 1, name: 'apple' });
+// function testFunc({ id, name }) {
+//   console.log(id);
+//   console.log(name);
+// }
+// testFunc({ id: 1, name: 'apple' });
 </script>
 
 <style>
